bae83b41c11b83a19d0b5ffc1d06534470df686c
FpapiezHC <160175850+FpapiezHC@users.noreply.github.com>
2025-02-09 00:27:16 -0600

    Message

--- app/src/main/java/org/example/Factorizer.java
+++ app/src/main/java/org/example/Factorizer.java
@@ -3,8 +3,19 @@ package org.example;
 import java.util.ArrayList;
 
 public class Factorizer {
-  public ArrayList<Integer> primeFactors(Integer n) {
-    // Implement your code here!
-    return new ArrayList<>();
-  }
+    public ArrayList<Integer> primeFactors(Integer n) {
+        ArrayList<Integer> factors = new ArrayList<>();
+        if (n <= 1) {
+            return factors; 
+        }
+        int divisor = 2; 
+        while (n > 1) {
+            while (n % divisor == 0) {
+                factors.add(divisor);
+                n /= divisor;
+            }
+            divisor++;
+        }
+        return factors;
+    }
 }
